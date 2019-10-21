75a70babace0839d3622329d6062f99bdbe26e3d
leHoangKhacSon <sonqueen.no1@gmail.com>
2019-10-22 06:05:16 +0700

    Create validate login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+import axios from 'axios';
 
 import './Login.css';
 
 function Login() {
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    if(email === '') {
+      console.log('required email');
+      return;
+    }
+    if(password === '') {
+      console.log('required password');
+      return;
+    }
+    const postData = async () => {
+      try {
+        const res = await axios.post('http://localhost:7000/api/user/login', {
+          email, password
+        });
+        console.log(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    postData();
+    console.log('login success');
+    setEmail('');
+    setPassword('');
+  }
+
   return (
-    <form action="" className="login-form">
+    <form onSubmit={handlerSubmit} className="login-form">
       <p className="login-form-title">
         Books ReactJS
       </p>
@@ -20,6 +50,8 @@ function Login() {
             type="text" 
             name="email" 
             placeholder="Email" 
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
           />
       </div>
       <div className="login-form-value">
@@ -28,6 +60,8 @@ function Login() {
           type="password" 
           name="password" 
           placeholder="Password" 
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
         />
       </div>
       
