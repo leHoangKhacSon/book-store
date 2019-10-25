baa9203ad93010ec3916c68993aaa8232a654121
leHoangKhacSon <sonqueen.no1@gmail.com>
2019-10-25 20:46:28 +0700

    Create register form auth with JWT

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import {
-  BrowserRouter as 
   Link
 } from "react-router-dom";
 import axios from 'axios';
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,44 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import './Signup.css';
 
-function Signup() {
+function Signup({ handlerValidateForm, handlerSubmitPost }) {
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ password2, setPassword2 ] = useState('');
+  const [ errors, setErrors ] = useState({
+    email: '',
+    password: '',
+    password2: ''
+  })
+  //handler submit 
+  const handlerSubmit = e => {
+    e.preventDefault();
+    handlerValidateForm(email, password, password2, setErrors);
+    handlerSubmitPost(email, password, setErrors);
+  }
+
   return (
-    <form action="" class="login-form">
-      <p class="login-form-title">
+    <form onSubmit={handlerSubmit} className="login-form">
+      <p className="login-form-title">
         Books ReactJS
       </p>
-      <div class="login-form-value">
+      <div className="login-form-value">
         <img src="/user.png" alt="" />
         <input 
           type="text" 
           name="email" 
           placeholder="Email" 
+          value={email}
+          onChange={e => setEmail(e.target.value)}
         />
       </div>
-      <div class="login-form-value">
+      <p className="error-message">{errors.email}</p>
+      <div className="login-form-value">
         <img src="/password.png" alt="" />
         <input 
           type="password" 
           name="password" 
           placeholder="Password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
         />
       </div>
-      <div class="login-form-value">
+      <p className="error-message">{errors.password}</p>
+      <div className="login-form-value">
         <img src="/password.png" alt="" />
         <input 
           type="password" 
           name="re-password" 
           placeholder="Re-enter Password"
+          value={password2}
+          onChange={e => setPassword2(e.target.value)}
         />
       </div>
+      <p className="error-message">{errors.password2}</p>
       <input 
         type="submit" 
         value="Sign up" 
-        class="login-form-button"
+        className="login-form-button"
       />
-      <div class="login-form-footer">
+      <div className="login-form-footer">
         You already have account ?
         <Link to="/login/" className="sign-up-button">
           Login
@@ -47,5 +72,7 @@ function Signup() {
     </form>
   )
 }
+
+
 
 export default Signup
